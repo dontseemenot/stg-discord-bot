597f8b8f644a1e5971a0576dab45a31b77f72932
arthur <artsze10@gmail.com>
2023-10-29 23:11:33 +1100

    bot commands and github actions

--- src/main/java/org/example/BotListeners.java
+++ src/main/java/org/example/BotListeners.java
@@ -7,11 +7,20 @@ import org.jetbrains.annotations.NotNull;
 public class BotListeners extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        System.out.println("A message has been sent.");
-
         if (!event.getAuthor().isBot()) {
-            String messageSent = event.getMessage().getContentRaw();
-            event.getChannel().sendMessage("Message: " + messageSent).queue();
+            String messageSent = event.getMessage().getContentRaw().toLowerCase();
+            if (messageSent.startsWith("/bot ")) {
+                if (messageSent.equals("/bot ping")) {
+                    event.getChannel().sendMessage("Pong!").queue();
+                } else if (messageSent.startsWith("/bot say ")) {
+                    String repeatMessage = messageSent.substring(9);
+                    event.getChannel().sendMessage(repeatMessage).queue();
+                }
+                else {
+                    event.getChannel().sendMessage("Commands:\n/bot ping\n/bot say <message>").queue();
+                }
+
+            }
         }
 
     }

e085a270e0c2ae7b3ac2e3f87627d713fe2c95b6
arthur <artsze10@gmail.com>
2023-10-30 16:46:44 +1100

    Slash commands and ChatGPT initial implementation

--- src/main/java/org/example/BotListeners.java
+++ src/main/java/org/example/BotListeners.java
@@ -1,28 +1,64 @@
 package org.example;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Properties;
+
 public class BotListeners extends ListenerAdapter {
-    @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if (!event.getAuthor().isBot()) {
-            String messageSent = event.getMessage().getContentRaw().toLowerCase();
-            if (messageSent.startsWith("/bot ")) {
-                if (messageSent.equals("/bot ping")) {
-                    event.getChannel().sendMessage("Pong!").queue();
-                } else if (messageSent.startsWith("/bot say ")) {
-                    String repeatMessage = messageSent.substring(9);
-                    event.getChannel().sendMessage(repeatMessage).queue();
-                }
-                else {
-                    event.getChannel().sendMessage("Commands:\n/bot ping\n/bot say <message>").queue();
-                }
 
-            }
-        }
-
+    private ChatGPTAPI chatGPTAPI;
+    public BotListeners() {
+        Properties properties = PropertiesHandler.GetProperties();
+        String chatGPTApiKey = properties.getProperty("CHATGPT_APIKEY");
+        chatGPTAPI = new ChatGPTAPI(chatGPTApiKey);
+        System.out.println("Botlisteners setup");
     }
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        switch (event.getName()) {
+            case "ping":
+                long time = System.currentTimeMillis();
+                // Ephermal: only the user can see the command
+                event.reply("Pong!").setEphemeral(true) // reply or acknowledge
+                        .flatMap(v ->
+                                event.getHook().editOriginalFormat("Pong! %d ms", System.currentTimeMillis() - time) // then edit original
+                        ).queue(); // Queue both reply and edit
+                break;
+            case "say":
+                event.reply(event.getOption("message").getAsString()).queue(); // reply immediately
+                break;
+            case "gpt":
+//                String prompt = event.getOption("message").getAsString();
+//                String response = chatGPTAPI.sendPrompt(prompt);
+//                event.getChannel().sendMessage(response).queue();
+                event.getChannel().sendMessage("Sorry, this feature is still in development.").queue();
+                break;
+            default:
+                event.getChannel().sendMessage("Commands:\n/ping\n/say <message>").queue();
+                break;
+        }
+    }
+//    @Override
+//    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+//        if (!event.getAuthor().isBot()) {
+//            String messageSent = event.getMessage().getContentRaw().toLowerCase();
+//            if (messageSent.startsWith("/ ")) {
+//                if (messageSent.equals("/ping")) {
+//                    event.getChannel().sendMessage("Pong!").queue();
+//                } else if (messageSent.startsWith("/say ")) {
+//                    String repeatMessage = messageSent.substring(5);
+//                    event.getChannel().sendMessage(repeatMessage).queue();
+//                }
+//                else {
+//                    event.getChannel().sendMessage("Commands:\n/ping\n/say <message>").queue();
+//                }
+//
+//            }
+//        }
+//
+//    }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
@@ -14,11 +16,20 @@ public class Main {
     public static void main(String[] args) throws LoginException {
         Properties properties = PropertiesHandler.GetProperties();
         String botToken = properties.getProperty("BOT_TOKEN");
+
         JDA bot = JDABuilder.createDefault(botToken)
-                .setActivity(Activity.playing("Playing around"))
+                .setActivity(Activity.playing("with your mum"))
                 .addEventListeners(new BotListeners())
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
+
+        bot.updateCommands().addCommands(
+                Commands.slash("ping", "Calculate ping of the bot"),
+                Commands.slash("say", "Bot will repeat the message")
+                        .addOption(OptionType.STRING, "message", "Message to repeat", true),
+                Commands.slash("gpt", "Send a prompt to ChatGPT")
+                        .addOption(OptionType.STRING, "message", "ChatGPT prompt", true)
+        ).queue();
     }
 
 }
